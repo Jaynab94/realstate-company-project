ff36cb7119390552539d91372451b953555bb207
unknown <anisamim94@gmail.com>
2024-04-16 01:02:12 +0600

    initialized real state project

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBLHdK8-u4F5Db0SMmf8XlhNvCEg3TA_n0",
+    authDomain: "real-state-project-7b266.firebaseapp.com",
+    projectId: "real-state-project-7b266",
+    storageBucket: "real-state-project-7b266.appspot.com",
+    messagingSenderId: "845985722535",
+    appId: "1:845985722535:web:7b7f60671b742b30b5eb82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
